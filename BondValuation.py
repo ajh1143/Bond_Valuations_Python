9d3d0c3977345bde3c36c6fd9588bcb53e579ecd
Andrew Holt <15341747+ajh1143@users.noreply.github.com>
2018-01-28 13:28:18 -0800

    Update BondValuation.py

--- BondValuation.py
+++ BondValuation.py
@@ -11,18 +11,27 @@ from pprint import pprint
 
 def bond_price(par_val, coup_rate, ttm, yld):
 
+#Create empty Dictionary to pass into Pandas DataFrame
     data = {'Cash_Flow': [],
                  'Time': [],
             'PV_Factor': [],
                    'PV': []
            }
+    #Create DataFrame
     df = pd.DataFrame(data, columns=['Cash_Flow', 'Time', 'PV_Factor', 'PV'])
+    #Generate Cash_Flow content
     cash_flow = np.array(np.repeat(int(par_val*coup_rate), ttm-1))
+    #Push Cash_Flow records into DataFrame
     df.Cash_Flow = np.append(cash_flow, int(par_val*(1+coup_rate)))
+    #Create Time record representing years, add to DataFrame
     df.Time = df.index+1
+    #Create PV Factor, add to DataFrame
     df.PV_Factor = np.array(1/(1+yld)**df['Time'])
+    #Create PV record and add to DataFrame
     df.PV = np.array(df.Cash_Flow * df.PV_Factor)
+    #Set index to Time(Years)
     df.set_index('Time', inplace=True)
+    #Return DataFrame and sum of PV column
     return df, sum(df.PV)
 
 # Example set with par value = $100, coupon rate = %5, time to maturity = 5 years, and yield = %6
